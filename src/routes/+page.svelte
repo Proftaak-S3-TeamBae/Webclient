3402681f95eb0710b43d51729f1e6752d1753990
ComLarsic <larsjanjensen@gmail.com>
2023-09-18 10:28:04 +0200

    Add barebones sveltekit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import logo from "$lib/images/logo.webp";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Project Bae" />
+</svelte:head>
+
+<section>
+	<img src={logo} alt="Logo" />
+	<h1>Welcome to project Bae ;P</h1>
+</section>
+
+<style>
+</style>
